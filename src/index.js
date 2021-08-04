8fb110a4709fbad0f62937b3e1c725b10ff68e98
Sonali Rai <sonali491@gmail.com>
2021-08-04 14:57:51 +0200

    Updated webcomponent package name

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { defineCustomElements } from 'scania-webcomponents/dist/loader';
+import { defineCustomElements } from 'webcomponents-package/dist/loader';
 
 ReactDOM.render(
   <React.StrictMode>
